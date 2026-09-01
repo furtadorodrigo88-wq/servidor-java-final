0c6368d2ae7dda6bc7dc47187db285a658a6d5f3
Wilson Furtado <furtadorodrigo88@gmail.com>
2026-09-01 10:10:13 -0100

    liberando asseco ao localhost

--- src/main/java/com/labanta/servidorlocal/security/SecurityConfig.java
+++ src/main/java/com/labanta/servidorlocal/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000","http://labanta.org"));
+        config.setAllowedOrigins(List.of("http://localhost:3000","http://labanta.org","http://localhost:5173"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowCredentials(true);
         config.setAllowedHeaders(List.of("Authorization", "Content-type"));
